02986d92e4d689cb4b50a210a770027dc1eec18d
alanttran <mr.alan.tran@gmail.com>
2017-09-14 00:16:01 -0700

    cleaned up the templates

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,21 +1,9 @@
 import React from "react";
-import "./main.css";
+import "./signup.css";
 
-import SearchPage from "../../pages/search";
-import Saved from "../saved/saved";
-
-// Using the datalist element we can create autofill suggestions based on the props.breeds array
-const Main = props =>
-  <div className="container">
-
-    <div className="jumbotron">
-      <h1 className="text-center"><strong><i className="glyphicon glyphicon-globe"></i> New York Times Search</strong></h1>
-    </div>
-
-    <SearchPage/>
-    
-    <Saved/>
-
+const Signup = props =>
+  <div className="">
+  	Signup
   </div>;
 
-export default Main;
+export default Signup;
