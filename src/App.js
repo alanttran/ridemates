03d58e05672428a7e23e707551d2cdde5ee67a88
JayaArasalike <mail@jayalaxmi.info>
2017-09-15 22:40:54 -0700

    working components login, signup, profile and main

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Login from './components/login';
+import Signup from './components/signup';
+import Profile from './components/profile';
+import Main from './components/main';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Main />
+        <Login />
+        <Signup />
+        <Profile />
       </div>
     );
   }
