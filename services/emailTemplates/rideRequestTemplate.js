63280206f67cdd1686ade833b33617dd179a2aea
Fabio <heloflyer@hotmail.com>
2017-09-16 16:59:02 -0700

    removed keys

--- services/emailTemplates/rideRequestTemplate.js
+++ services/emailTemplates/rideRequestTemplate.js
@@ -5,5 +5,21 @@
 // ============================================================================
 
 module.exports = (email) => {
-	return '<div> + email.body + </div>'
+	return `
+		<html>
+			<body>
+				<div style="text-align: center;">
+					<h3>Let's ride!</h3>
+					<p>Please answer:</p>
+					<p>${email.body}</p>
+					<div>
+						<a href="http://localhost:4000">Yes</a>
+					</div>
+					<div>
+						<a href="http://localhost:4000">No</a>
+					</div>
+				</div>
+			</body>
+		</html>
+	`;
 };
