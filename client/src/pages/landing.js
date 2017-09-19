06c61656466e92f701dbb9dd0e5f17b6980f55ca
alanttran <mr.alan.tran@gmail.com>
2017-09-19 00:12:49 -0700

    landing page update

--- client/src/pages/landing.js
+++ client/src/pages/landing.js
@@ -7,6 +7,16 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Typography from 'material-ui/Typography';
 import IconButton from 'material-ui/IconButton';
+import Grid from 'material-ui/Grid';
+
+import './landing.css';
+
+let imgUrl = './images/biking-cover.png';
+
+let coverImage = {
+	backgroundImage: 'url(' + imgUrl + ')',
+	backgroundRepeat: 'no-repeat'
+}
 
 class LandingPage extends Component{
 	render(){
@@ -14,14 +24,45 @@ class LandingPage extends Component{
 			<div>
 				<AppBar position="static">
 					<Toolbar>
-						<Typography type="title" color="inherit" >
+						<Typography className="rm-flex-auto" type="title" color="inherit" >
 							RideMates
 						</Typography>
-						<Button color="contrast">Login</Button>
+						<Button className="rm-login-button" color="contrast">Login</Button>
 					</Toolbar>
 				</AppBar>
-				<img src={'./images/biking.jpeg'} alt="biking"/>
-				<Main></Main>
+				<div style={coverImage} className="rm-cover-container">
+					<Typography className="rm-cover-title" type="display3" >
+							Find friends to ride with!
+					</Typography>
+					<Main></Main>
+				</div>
+				<Grid container spacing={0}>
+					<Grid className="rm-landing-page-items" item xs={6} sm={3}>
+						<Typography className="rm-text-align-center" type="display1" color="inherit" >
+							Efficient
+						</Typography>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit tempore consequuntur atque sit voluptatibus in voluptas distinctio quas est, perferendis, sunt assumenda. Eius voluptatum, magnam minus! Repellat quaerat aperiam itaque.</p>
+					</Grid>
+					<Grid className="rm-landing-page-items" item xs={6} sm={3}>
+						<Typography className="rm-text-align-center" type="display1" color="inherit" >
+							Friendly
+						</Typography>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit tempore consequuntur atque sit voluptatibus in voluptas distinctio quas est, perferendis, sunt assumenda. Eius voluptatum, magnam minus! Repellat quaerat aperiam itaque.</p>
+					</Grid>
+					<Grid className="rm-landing-page-items" item xs={6} sm={3}>
+						<Typography className="rm-text-align-center" type="display1" color="inherit" >
+							Fast
+						</Typography>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit tempore consequuntur atque sit voluptatibus in voluptas distinctio quas est, perferendis, sunt assumenda. Eius voluptatum, magnam minus! Repellat quaerat aperiam itaque.</p>
+					</Grid>
+					<Grid className="rm-landing-page-items" item xs={6} sm={3}>
+						<Typography className="rm-text-align-center" type="display1" color="inherit" >
+							RideMates
+						</Typography>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit tempore consequuntur atque sit voluptatibus in voluptas distinctio quas est, perferendis, sunt assumenda. Eius voluptatum, magnam minus! Repellat quaerat aperiam itaque.</p>
+					</Grid>
+				</Grid>
+				
 			</div>
 			
 		)
