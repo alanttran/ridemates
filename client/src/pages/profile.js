44892aea6638a3690ec341db4f6eaef4fb441301
alanttran <mr.alan.tran@gmail.com>
2017-09-30 01:43:14 -0700

    profile page bg

--- client/src/pages/profile.js
+++ client/src/pages/profile.js
@@ -6,17 +6,21 @@ import './profile.css';
 let imgUrl = './images/bike-background-02.png';
 
 let bgImage = {
-  backgroundImage: 'url(' + imgUrl + ')'
+  backgroundImage: 'url(' + imgUrl + ')',
+  minHeight:'100%',
+  width:'100%'
 }
 
 class ProfilePage extends Component{
 	render(){
 		return(
-			<div style={bgImage} className="rm-profile-container">
-				<div className="rm-profile-left-panel">
-					<Profile history={this.props.history} parent={this.props.parent}></Profile>
+			<div style={bgImage}>
+				<div  className="rm-profile-container">
+					<div className="rm-profile-left-panel">
+						<Profile history={this.props.history} parent={this.props.parent}></Profile>
+					</div>
+					<img className="" src="./images/biker-04.png"/>
 				</div>
-				<img className="" src="./images/biker-04.png"/>
 			</div>
 		)
 		
